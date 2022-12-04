6263f1d9595f71bdf716e2ffbcb544296a44fada
Justin Cook <jcook03266@gmail.com>
2022-12-04 03:00:04 -0500

    Package refactor

--- Package.swift
+++ Package.swift
@@ -5,27 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "Sheathed-TextField-SwiftUI",
-    platforms: [
-        .iOS(.v16)
-    ],
-    products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "Sheathed-TextField-SwiftUI",
-            targets: ["Sheathed-TextField-SwiftUI"]),
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "Sheathed-TextField-SwiftUI",
-            dependencies: []),
-        .testTarget(
-            name: "Sheathed-TextField-SwiftUITests",
-            dependencies: ["Sheathed-TextField-SwiftUI"]),
-    ]
+    platforms: [.iOS(.v16)],
+    products: [.library(name: "Sheathed-TextField-SwiftUI", targets: ["Sheathed-TextField-SwiftUI"])],
+    targets: [.target(name: "Sheathed-TextField-SwiftUI", path: "Sources")]
 )
